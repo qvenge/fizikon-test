ca6b726b10c6db2ac717b907cd2faf821cd35728
qvenge <coovenbm@gmail.com>
2021-04-26 01:02:27 +0700

    add button component

---
+++ src/components/button/button.js
@@ -0,0 +1,31 @@
+import './button.css';
+
+export class Button {
+  constructor(props = {}) {
+    this._className = 'button';
+    this._elem = null;
+    this._init(props);
+  }
+
+  getElem() {
+    return this._elem;
+  }
+
+  _init(props) {
+    this._elem = document.createElement('button');
+    this._elem.className = this._className;
+    this._elem.textContent = props.text ?? 'Button';
+
+    if (typeof props.onClick === 'function') {
+      this._elem.addEventListener('click', () => props.onClick.call(null, this));
+    }
+  }
+
+  getText() {
+    return this._elem.textContent;
+  }
+
+  setText(value) {
+    this._elem.textContent = value;
+  }
+}
